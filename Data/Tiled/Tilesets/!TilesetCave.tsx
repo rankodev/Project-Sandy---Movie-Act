b4cf79dc2679c846d5b76f80e1ab3fc995276d62
Mashirosakura <72957526+Mashirosakura@users.noreply.github.com>
2026-07-13 21:39:14 +0100

    Town!

---
+++ Data/Tiled/Tilesets/!TilesetCave.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="!TilesetCave" tilewidth="32" tileheight="32" tilecount="1592" columns="8">
+ <image source="../Assets/!TilesetCave.png" width="256" height="6368"/>
+</tileset>
---
+++ Data/Tiled/Tilesets/ekat_cave.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="ekat-cave" tilewidth="32" tileheight="32" tilecount="1592" columns="8">
+ <image source="../Assets/!TilesetCave.png" width="256" height="6368"/>
+</tileset>
