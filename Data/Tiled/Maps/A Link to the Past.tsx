b0761278cec5af91b032cb6a027969ef09f9ab5c
Mashirosakura <72957526+Mashirosakura@users.noreply.github.com>
2026-07-20 22:22:55 +0100

    Maps 20/07/26
    Added lanterns to Beach Cave and outside
    Added lanterns to Dustbone Town
    Fixed buildings not appearing in Dustbone Town
    Added interior maps for Dustbone Town

---
+++ Data/Tiled/Maps/A Link to the Past.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="A Link to the Past" tilewidth="32" tileheight="32" tilecount="28" columns="4">
+ <image source="../Assets/A Link to the Past.png" width="128" height="224"/>
+</tileset>
---
+++ Data/Tiled/Tilesets/sandy_outside.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="sandy_outside" tilewidth="32" tileheight="32" tilecount="7496" columns="8">
+ <image source="../Assets/project-sandy outside.png" trans="ff00ff" width="256" height="30000"/>
+</tileset>
