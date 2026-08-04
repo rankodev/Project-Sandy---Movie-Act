aad484ac9a3a7b8ee08d7345aa21b0f0d420a119
Ranko <rankodev12@gmail.com>
2026-08-04 12:30:16 -0400

    Fixing Mashi Maps

---
+++ Data/Tiled/Tilesets/Haunted Woods (Normal Pallet).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="Haunted Woods (Normal Pallet)" tilewidth="32" tileheight="32" tilecount="64" columns="4">
+ <image source="../Assets/Haunted Woods (Normal Pallet).png" width="128" height="512"/>
+</tileset>
--- Data/Tiled/Tilesets/Pond and Bridge Formatted.tsx
+++ Data/Tiled/Tilesets/Pond and Bridge Formatted.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="Pond and Bridge Formatted" tilewidth="32" tileheight="32" tilecount="238" columns="17">
- <image source="../Assets/Pond and Bridge Formatted.png" trans="ff00ff" width="544" height="448"/>
+<tileset version="1.10" tiledversion="1.12.2" name="Pond and Bridge Formatted" tilewidth="32" tileheight="32" tilecount="238" columns="17">
+ <image source="../Assets/Pond and Bridge Formatted.png" width="544" height="448"/>
 </tileset>
